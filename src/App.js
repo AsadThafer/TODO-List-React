0d2fc86d8e5754e559af9e9827576500ccd641a4
Asad Asad <asad.gamerz@gmail.com>
2022-12-21 14:38:17 +0200

    still working on adding task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from './components/Navbar.js';
 import Task from './components/Tasks/Task.js';
+import AddTask from './components/Tasks/AddTask.js';
+import './App.css';
 function App() {
   return (
     <div>
       <Navbar />
+      <AddTask
+      {...{ onAddTask: (task) =>  console.log(task)
+          }}
+      />
       <ul className='Task-list'>
       <Task {...{
         status: 'TO-DO',
@@ -22,6 +28,13 @@ function App() {
         title: 'Task 3',
         Assignee: 'Asad Asad',
       }}
+        />
+      <Task {...{
+        status: 'TO-DO',
+        title: 'Task 4',
+        Assignee: 'Asad Asad',
+      }}
+
        />
        </ul>
 
---
+++ src/components/Tasks/AddTask.js
@@ -0,0 +1,62 @@
+import './AddTask.css';
+import React, { useState } from 'react';
+import Button from '../Button.js';
+
+function AddTask(props) {
+    const [title, setTitle] = useState('');
+    const [Assignee, setAssignee] = useState('');
+    const [status, setStatus] = useState('TO-DO');
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleAssigneeChange = (event) => {
+        setAssignee(event.target.value);
+    };
+
+    const handleStatusChange = (event) => {
+        setStatus(event.target.value);
+    };
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.onAddTask({ title, Assignee, status });
+        setStatus('TO-DO');
+        setTitle('');
+        setAssignee('');
+    };
+        
+
+
+    return (
+        <form className='startAddTask' onSubmit={handleSubmit}>
+            <input
+                type='text'
+                placeholder='Title'
+                value={title}
+                onChange={handleTitleChange}
+            />
+            <input
+                type='text'
+                placeholder='Assignee'
+                value={Assignee}
+                onChange={handleAssigneeChange}
+            />
+            <select
+                value={status}
+                onChange={handleStatusChange}
+            >
+                <option value='TO-DO'>TO-DO</option>
+                <option value='DONE'>DONE</option>
+            </select>
+            
+            
+            <Button className='btn btn--add'
+             type='submit'>Add Task</Button>
+        </form>
+    );
+}
+
+export default AddTask;
